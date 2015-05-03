b5b1dea3e68b711e664b665d8ddeffa40daaca8b
Joe Norris <yourboybstar@gmail.com>
2015-05-02 17:23:09 -0700

    changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+function fizzBuzzer(total) {
+  var n = 1
+  function createTag(parent, child, value, classy) {
+    var parent = document.querySelector(parent);
+    var child = parent.appendChild(document.createElement(child));
+    var value = child.innerHTML = value;
+    if(typeof classy !== "undefined") {
+        var classy = child.classList.add(value);
+    }
+
+  };
+  function check(n) {
+    var msg = '';
+    if ( n % 3 == 0 ) { msg += "fizz" };
+    if ( n % 5 == 0 ) { msg += "buzz" };
+    return msg || n;
+  }
+
+  for (n; n <= total; n++) {
+    createTag(".panel", "div", check(n),check(n));
+  }
+}
+fizzBuzzer(100);
